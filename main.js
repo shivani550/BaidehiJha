c86b4fe5e8102638b973fa00a626a4b23b61b805
Shivani Jha <81892912+shivani550@users.noreply.github.com>
2025-05-27 03:37:54 +0530

    Create main.js
    Added the javascript code for toggling.

---
+++ main.js
@@ -0,0 +1,14 @@
+let search = document.querySelector('.search-box');
+document.querySelector('#search-icon').onclick = () =>{
+    search.classList.toggle('active');
+    cart.classList.remove('active');
+    
+};
+let cart = document.querySelector('.cart');
+document.querySelector('#cart-icon').onclick = () =>{
+    cart.classList.toggle('active');
+    search.classList.remove('active');
+
+
+
+};
