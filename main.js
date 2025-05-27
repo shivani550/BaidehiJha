31491b8ef9413f3aa9c4bf40bdc7316e34f1306e
Shivani Jha <81892912+shivani550@users.noreply.github.com>
2025-05-28 01:11:06 +0530

    Update main.js
    ADDED REST OF THE ELEMENTS OF THE WEBSITE

--- main.js
+++ main.js
@@ -8,7 +8,43 @@ let cart = document.querySelector('.cart');
 document.querySelector('#cart-icon').onclick = () =>{
     cart.classList.toggle('active');
     search.classList.remove('active');
-
-
-
 };
+let user = document.querySelector('.user');
+document.querySelector('#user-icon').onclick = () =>{
+    user.classList.toggle('active');
+    cart.classList.remove('active');
+    search.classList.remove('active');
+}
+var swiper = new Swiper(".new-arrival", {
+    spaceBetween: 20,
+    loop:true,
+    centeredSlides: true,
+    autoplay:{
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerview: 0,
+        },
+        568: {
+            slidesPerview: 2,
+        },
+        768: {
+            slidesPerview: 2,
+        },
+        1020:{
+            slidesPerview: 3,
+        },
+    
+
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEL:".swiper-button-next",
+        prevEL:".swiper-button-prev",
+    },
+});
