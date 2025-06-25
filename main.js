00a0784fc532ee6b2ce4ffe943c4a62c45d55acb
Shivani Jha <shivanijhavats@gmail.com>
2025-06-26 00:38:47 +0530

    FIXED SXROLLING

---
+++ main.js
@@ -0,0 +1,81 @@
+let search = document.querySelector('.search-box');
+document.querySelector('#search-icon').onclick = () =>{
+    search.classList.toggle('active');
+    cart.classList.remove('active');
+
+    user.classList.remove('active');
+    
+}
+
+    
+
+let cart = document.querySelector('.cart');
+document.querySelector('#cart-icon').onclick = () =>{
+    cart.classList.toggle('active');
+    search.classList.remove('active');
+
+    user.classList.remove('active');
+}
+
+
+
+
+let user = document.querySelector('.user');
+document.querySelector('#user').onclick = () =>{
+    user.classList.toggle('active');
+    cart.classList.remove('active');
+    search.classList.remove('active');
+}
+ var swiper = new Swiper(".new-arrival", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      480: {
+        slidesPerView: 1,
+      },
+    },
+  });
+
+
+
+/*var swiper = new Swiper(".new-arrival", {
+    spaceBetween: 20,
+    loop:true,
+    centeredSlides: true,
+    autoplay:{
+        delay: 5500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 0,
+        },
+        568: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1020:{
+            slidesPerView: 3,
+        },
+    
+
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev",
+    },
+});*/
